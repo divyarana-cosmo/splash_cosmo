4a0e1a0056d6b6c4c6d2e3c0ae33864ee5c1bd4d
divyarana-cosmo <divyarana757@gmail.com>
2025-06-01 20:09:42 +0200

    updated

--- sim_hmf_cut.py
+++ sim_hmf_cut.py
@@ -4,18 +4,25 @@ from colossus.cosmology import cosmology
 from colossus.lss import mass_function
 from colossus.halo import mass_so
 
+
 def get_r200m(omgm, sigma8):
-    #setup the cosmology
+    # Setup the cosmology
     params = {'flat': True, 'H0': 100, 'Om0': omgm, 'Ob0': 0.049, 'sigma8': sigma8, 'ns': 0.95}
     cosmo = cosmology.setCosmology('myCosmo', **params)
-    #get the average halo mass
-    xmh      = np.linspace(1e14,1e18,500)
-    mfunc_so = mass_function.massFunction(xmh, 0.0, mdef = '200m', model = 'tinker08')
-    avg_mh   = sum(mfunc_so*xmh)/sum(mfunc_so)
-    #convert it to r200m
-    return mass_so.M_to_R(avg_mh, z=0.0, mdef='200m')/1e3
 
-print(get_r200m(0.3,0.8))
+    # Use log-spaced mass array for better resolution
+    xmh = np.logspace(14, 18, 500)
+
+    # Get dn/dlnM
+    mfunc_so = mass_function.massFunction(xmh, 0.0, mdef='200m', model='tinker08', q_out='dndlnM')
+
+    # Compute average mass using proper integration over ln(M)
+    lnM = np.log(xmh)
+    dlnM = np.gradient(lnM)
+    avg_mh = np.sum(mfunc_so * xmh * dlnM) / np.sum(mfunc_so * dlnM)
+
+    # Convert mass to r200m and return in h⁻¹ Mpc
+    return mass_so.M_to_R(avg_mh, z=0.0, mdef='200m') / 1e3
 
 omgmarr     = np.linspace(0.15, 0.45,20)
 sigma8arr   = np.linspace(0.65,1.05,20)
